930e7608c924f819f1c8d5fdf8c0f48efdbef2e3
kensuke goto <utogoken2@yahoo.co.jp>
2019-10-12 00:29:01 +0900

    [追加] トレーニングメニューのリストを表示する

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,41 @@
-import React from "react"
+import React,{ Component } from "react"
 import { Header, Footer } from "./Layouts"
 import Exercises from "./Exercises"
+import {
+  muscles,
+  exercises
+} from "../store"
 
-export default props => 
-  <div className="App">
-    <Header />
-    <Exercises />
-    <Footer />
-  </div>
+export default class extends Component {
+
+  state = {
+    exercises
+  }
+
+  getExercisesByMuscles(){
+  
+    return Object.entries(this.state.exercises.reduce((exercises,exercise) => {
+        const { muscles } = exercise
+        exercises[muscles] = exercises[muscles]
+          ? [...exercises[muscles],exercise]
+          : [exercise]
+        return exercises
+      },{})
+    )
+  }
+
+  render(){
+    return <>
+      <Header />
+
+      <Exercises 
+        exercises={this.getExercisesByMuscles()}
+      />
+
+      <Footer 
+        muscles={muscles}
+      />
+    </>
+  }
+
+}
--- src/components/Exercises/index.js
+++ src/components/Exercises/index.js
@@ -1,25 +1,64 @@
 import React from "react"
 import {
   Grid,
-  Paper
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
 } from "@material-ui/core"
-import LeftPane from "./LeftPane"
-import RightPane from "./RightPane"
+
 
 const styles = {
   Paper: {
     padding: 20,
     marginTop: 10,
-    marginBottom: 10
+    marginBottom: 10,
+    height: 500,
+    overflowY: "auto"
   }
 }
 
-export default props =>
+export default ({ exercises }) =>
   <Grid container spacing={2}>
     <Grid item sm>
-      <LeftPane styles={styles} />
+      <Paper style={styles.Paper}>
+        {
+          exercises.map(([group,exercises],key)=>
+            <Typography
+              variant="h5"
+              style={{textTransform: "capitalize"}}
+              key={key}
+            >
+              {group}
+              <List component="nav" aria-label="secondary mailbox folders">
+                {exercises.map(({title})=>{
+                
+                  return <ListItem button>
+                    <ListItemText primary={title} />
+                  </ListItem>
+                })}
+
+              </List>
+            </Typography>
+          )
+        }
+
+      </Paper>
     </Grid>
     <Grid item sm>
-      <RightPane styles={styles} />
+      <Paper style={styles.Paper}>
+        <Typography
+          variant="h5"
+        >
+          Welcome!
+        </Typography>
+        <Typography
+          variant="body1"
+          style={{marginTop:20}}
+        >
+          Please select in exercise from the list on the left
+        </Typography>
+      </Paper>
     </Grid>
   </Grid>
